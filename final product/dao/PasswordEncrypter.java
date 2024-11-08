a7dd4da85dd8b4963c7763fa5e7fa4ee90856b25
Trravo <166054427+Trravo@users.noreply.github.com>
2024-11-08 00:40:09 -0600

    Create PasswordEncrypter.java

---
+++ final product/dao/PasswordEncrypter.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PasswordEncrypter{
+    public static String encryptPassword(String password){
+        String result = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] mdRet = md.digest(password.getBytes());
+            result = Base64.getEncoder().encodeToString(mdRet);
+        }catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(PasswordEncrypter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+}
